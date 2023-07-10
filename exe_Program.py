e575c7357b606451658a2e910b3ed186962cfb52
Friebay <frybaylt@gmail.com>
2023-07-10 10:41:26 +0300

    Update exe_Program.py

--- exe_Program.py
+++ exe_Program.py
@@ -3,7 +3,6 @@ import json
 import requests
 import numpy as np
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
